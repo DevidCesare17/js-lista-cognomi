42d9a44f39904e14f2dff0e161339d4df3462147
Davide Cesare <davidereaper@gmail.com>
2020-08-25 14:57:43 +0200

    Fine esercizio

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// Lista Cognomi
+// Chiedere all’utente il cognome
+// inserirlo in un array con altri cognomi: ‘Bianchi’, ‘Rossi’, ‘Duzioni’, ‘Balsano’, ‘Verdi’
+// stampa la lista ordinata alfabeticamente
+// scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
+
+// 1. array cognomi default
+var cognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
+
+// 2. chiedere all'utente di inserire il suo cognome
+do {
+  var inputUtente = prompt("Inserisci il tuo cognome");
+  inputUtente = inputUtente.charAt(0).toUpperCase() + inputUtente.slice(1);
+} while (!isNaN(inputUtente));
+
+// 3. inserire il cognome dell'utente nell'array "cognomi"
+cognomi.push(inputUtente);
+
+// 4. stampa della lista ordinata alfabeticamente
+for (var i = 0; i < cognomi.length; i++) {
+  document.getElementById("lista").innerHTML += "<li>" + cognomi.sort()[i] + "</li>";
+}
+
+// 5. scrittura posizione esatta del cognome dell'utente nella Lista
+document.getElementById("posizione").innerHTML = cognomi.indexOf(inputUtente) + 1;
